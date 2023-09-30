1d19cd1c0153067afd9bd5df57cf53ebc82f31c1
owsley3 <owsley.iii@proton.me>
2023-10-01 00:18:02 +0400

    chore: update interfaces

--- contracts/interfaces/IIncentiveVoting.sol
+++ contracts/interfaces/IIncentiveVoting.sol
@@ -7,46 +7,54 @@ interface IIncentiveVoting {
         uint256 id;
         uint256 points;
     }
-
     struct LockData {
         uint256 amount;
-        uint256 weeksToUnlock;
+        uint256 epochsToUnlock;
     }
 
     event AccountWeightRegistered(
         address indexed account,
-        uint256 indexed week,
+        uint256 indexed epoch,
         uint256 frozenBalance,
         LockData[] registeredLockData
     );
-    event ClearedVotes(address indexed account, uint256 indexed week);
-    event NewVotes(address indexed account, uint256 indexed week, Vote[] newVotes, uint256 totalPointsUsed);
+    event ClearedVotes(address indexed account, uint256 indexed epoch);
+    event NewVotes(address indexed account, uint256 indexed epoch, Vote[] newVotes, uint256 totalPointsUsed);
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
     function clearRegisteredWeight(address account) external returns (bool);
 
     function clearVote(address account) external;
 
-    function getReceiverVotePct(uint256 id, uint256 week) external returns (uint256);
+    function getReceiverVotePct(uint256 id, uint256 epoch) external returns (uint256);
 
     function getReceiverWeightWrite(uint256 idx) external returns (uint256);
 
     function getTotalWeightWrite() external returns (uint256);
 
-    function registerAccountWeight(address account, uint256 minWeeks) external;
+    function registerAccountWeight(address account, uint256 minEpochs) external;
 
-    function registerAccountWeightAndVote(address account, uint256 minWeeks, Vote[] calldata votes) external;
+    function registerAccountWeightAndVote(address account, uint256 minEpochs, Vote[] calldata votes) external;
 
     function registerNewReceiver() external returns (uint256);
 
+    function renounceOwnership() external;
+
     function setDelegateApproval(address _delegate, bool _isApproved) external;
+
+    function transferOwnership(address newOwner) external;
 
     function unfreeze(address account, bool keepVote) external returns (bool);
 
     function vote(address account, Vote[] calldata votes, bool clearPrevious) external;
 
-    function MAX_LOCK_WEEKS() external view returns (uint256);
+    function EPOCH_LENGTH() external view returns (uint256);
 
-    function MAX_POINTS() external view returns (uint256);
+    function MAX_LOCK_EPOCHS() external view returns (uint256);
+
+    function MAX_PCT() external view returns (uint256);
+
+    function START_TIME() external view returns (uint256);
 
     function getAccountCurrentVotes(address account) external view returns (Vote[] memory votes);
 
@@ -54,33 +62,33 @@ interface IIncentiveVoting {
         address account
     ) external view returns (uint256 frozenWeight, LockData[] memory lockData);
 
+    function getEpoch() external view returns (uint256 epoch);
+
     function getReceiverWeight(uint256 idx) external view returns (uint256);
 
-    function getReceiverWeightAt(uint256 idx, uint256 week) external view returns (uint256);
+    function getReceiverWeightAt(uint256 idx, uint256 epoch) external view returns (uint256);
 
     function getTotalWeight() external view returns (uint256);
 
-    function getTotalWeightAt(uint256 week) external view returns (uint256);
-
-    function getWeek() external view returns (uint256 week);
+    function getTotalWeightAt(uint256 epoch) external view returns (uint256);
 
     function isApprovedDelegate(address owner, address caller) external view returns (bool isApproved);
+
+    function owner() external view returns (address);
 
     function receiverCount() external view returns (uint256);
 
     function receiverDecayRate(uint256) external view returns (uint32);
 
-    function receiverUpdatedWeek(uint256) external view returns (uint16);
+    function receiverEpochUnlocks(uint256, uint256) external view returns (uint32);
 
-    function receiverWeeklyUnlocks(uint256, uint256) external view returns (uint32);
+    function receiverUpdatedEpoch(uint256) external view returns (uint16);
 
     function tokenLocker() external view returns (address);
 
     function totalDecayRate() external view returns (uint32);
 
-    function totalUpdatedWeek() external view returns (uint16);
+    function totalEpochUnlocks(uint256) external view returns (uint32);
 
-    function totalWeeklyUnlocks(uint256) external view returns (uint32);
-
-    function vault() external view returns (address);
+    function totalUpdatedEpoch() external view returns (uint16);
 }
--- contracts/interfaces/ITokenLocker.sol
+++ contracts/interfaces/ITokenLocker.sol
@@ -9,19 +9,20 @@ interface ITokenLocker {
     }
     struct ExtendLockData {
         uint256 amount;
-        uint256 currentWeeks;
-        uint256 newWeeks;
+        uint256 currentEpochs;
+        uint256 newEpochs;
     }
 
-    event LockCreated(address indexed account, uint256 amount, uint256 _weeks);
-    event LockExtended(address indexed account, uint256 amount, uint256 _weeks, uint256 newWeeks);
+    event LockCreated(address indexed account, uint256 amount, uint256 _epochs);
+    event LockExtended(address indexed account, uint256 amount, uint256 _epochs, uint256 newEpochs);
     event LocksCreated(address indexed account, LockData[] newLocks);
     event LocksExtended(address indexed account, ExtendLockData[] locks);
     event LocksFrozen(address indexed account, uint256 amount);
     event LocksUnfrozen(address indexed account, uint256 amount);
     event LocksWithdrawn(address indexed account, uint256 withdrawn, uint256 penalty);
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
-    function extendLock(uint256 _amount, uint256 _weeks, uint256 _newWeeks) external returns (bool);
+    function extendLock(uint256 _amount, uint256 _epochs, uint256 _newEpochs) external returns (bool);
 
     function extendMany(ExtendLockData[] calldata newExtendLocks) external returns (bool);
 
@@ -31,49 +32,61 @@ interface ITokenLocker {
 
     function getTotalWeightWrite() external returns (uint256);
 
-    function lock(address _account, uint256 _amount, uint256 _weeks) external returns (bool);
+    function lock(address _account, uint256 _amount, uint256 _epochs) external returns (bool);
 
     function lockMany(address _account, LockData[] calldata newLocks) external returns (bool);
 
+    function renounceOwnership() external;
+
+    function setFeeReceiver(address _receiver) external returns (bool);
+
     function setPenaltyWithdrawalsEnabled(bool _enabled) external returns (bool);
+
+    function transferOwnership(address newOwner) external;
 
     function unfreeze(bool keepIncentivesVote) external;
 
-    function withdrawExpiredLocks(uint256 _weeks) external returns (bool);
+    function withdrawExpiredLocks(uint256 _epochs) external returns (bool);
 
     function withdrawWithPenalty(uint256 amountToWithdraw) external returns (uint256);
 
-    function MAX_LOCK_WEEKS() external view returns (uint256);
+    function EPOCH_LENGTH() external view returns (uint256);
 
-    function PRISMA_CORE() external view returns (address);
+    function LOCK_TO_TOKEN_RATIO() external view returns (uint256);
+
+    function MAX_LOCK_EPOCHS() external view returns (uint256);
+
+    function MAX_PCT() external view returns (uint256);
+
+    function START_TIME() external view returns (uint256);
+
+    function feeReceiver() external view returns (address);
 
     function getAccountActiveLocks(
         address account,
-        uint256 minWeeks
+        uint256 minEpochs
     ) external view returns (LockData[] memory lockData, uint256 frozenAmount);
 
     function getAccountBalances(address account) external view returns (uint256 locked, uint256 unlocked);
 
+    function getAccountIsFrozen(address account) external view returns (bool isFrozen);
+
     function getAccountWeight(address account) external view returns (uint256);
 
-    function getAccountWeightAt(address account, uint256 week) external view returns (uint256);
+    function getAccountWeightAt(address account, uint256 epoch) external view returns (uint256);
+
+    function getEpoch() external view returns (uint256 epoch);
 
     function getTotalWeight() external view returns (uint256);
 
-    function getTotalWeightAt(uint256 week) external view returns (uint256);
-
-    function getWeek() external view returns (uint256 week);
+    function getTotalWeightAt(uint256 epoch) external view returns (uint256);
 
     function getWithdrawWithPenaltyAmounts(
         address account,
         uint256 amountToWithdraw
     ) external view returns (uint256 amountWithdrawn, uint256 penaltyAmountPaid);
 
-    function guardian() external view returns (address);
-
     function incentiveVoter() external view returns (address);
-
-    function lockToTokenRatio() external view returns (uint256);
 
     function lockToken() external view returns (address);
 
@@ -81,9 +94,7 @@ interface ITokenLocker {
 
     function penaltyWithdrawalsEnabled() external view returns (bool);
 
-    function prismaCore() external view returns (address);
-
     function totalDecayRate() external view returns (uint32);
 
-    function totalUpdatedWeek() external view returns (uint16);
+    function totalUpdatedEpoch() external view returns (uint16);
 }
